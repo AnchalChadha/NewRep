fdfb91eafd9c65a50ec6400c2f3457f9c8a7b1ce
unknown <anchal.chadha@BSSDT018.betasoftsystems.com>
2015-04-15 14:58:55 +0530

    first commit

---
+++ src/ex2/testfile.java
@@ -0,0 +1,16 @@
+package ex2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class testfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub+
+		System.out.println("hello");
+		//System.setProperty("webdriver.chrome.driver","C:/Users/anchal.chadha/Desktop/chromedriver.exe");
+		//WebDriver driver=new ChromeDriver();
+	}
+
+}
